5b1b402e0053913d9d64332abf85c990cb7f658d
aleksandGG <aleksandrgaranin89@gmail.com>
2020-10-19 16:34:53 -0700

    ARREYS OBJECT FUNCTIONS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const defaultResult = 3
 
 let currentResult = defaultResult;
+let logEntries = [];
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
@@ -10,35 +11,57 @@ function getUserNumberInput() {
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+
     outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+    operatorItentifier,
+    prevResult,
+    operatorNumber,
+    newResult
+) {
+    const logEntry = {
+        operator: operatorItentifier,
+        prevResult: prevResult,
+        number: operatorNumber,
+        result: newResult
+    }
+    logEntries.push(logEntry)
+    console.log(logEntries)
 }
 
 function add() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
     currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+', initialResult , enteredNumber)
+    createAndWriteOutput('+', initialResult, enteredNumber)
+    writeToLog('ADD', initialResult, enteredNumber, currentResult)
+
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
     currentResult = currentResult - enteredNumber;
-    createAndWriteOutput('-', initialResult , enteredNumber)
+    createAndWriteOutput('-', initialResult, enteredNumber)
+    writeToLog('SUBTRUCT', initialResult, enteredNumber, currentResult)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
     currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/', initialResult , enteredNumber)
+    createAndWriteOutput('/', initialResult, enteredNumber)
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
     currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*', initialResult , enteredNumber)
+    createAndWriteOutput('*', initialResult, enteredNumber)
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click', add);
